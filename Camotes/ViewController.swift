03eac9c2f6020b86ed9a7ea67997bd3a6ad19432
ikeikeikeike <jp.ne.co.jp@gmail.com>
2018-04-16 02:14:31 +0900

    initia

---
+++ Camotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Camotes
+//
+//  Created by Tatsuo Ikeda on 2018/04/15.
+//  Copyright © 2018 Tatsuo Ikeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
