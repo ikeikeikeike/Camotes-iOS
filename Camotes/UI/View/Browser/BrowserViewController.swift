b6af18c34726709dbc5dca789d4be87744fdb0e7
ikeikeikeike <jp.ne.co.jp@gmail.com>
2018-04-19 00:59:29 +0900

    tiny browser for downloading file

--- Camotes/UI/View/Browser/BrowserViewController.swift
+++ Camotes/UI/View/Browser/BrowserViewController.swift
@@ -9,39 +9,119 @@
 import UIKit
 import WebKit
 
-class BrowserViewController: UIViewController, WKNavigationDelegate {
+class BrowserViewController: UIViewController, WKNavigationDelegate, UITextFieldDelegate {
+    
+    let defaultURL = "https://google.com"
 
     @IBOutlet weak var browser: WKWebView!
     @IBOutlet weak var searchText: UITextField!
     
+    @IBOutlet weak var backButton: UIBarButtonItem!
+    @IBOutlet weak var forwardButton: UIBarButtonItem!
+    @IBOutlet weak var reloadButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        browser.navigationDelegate = self
+        backButton.isEnabled = false
+        forwardButton.isEnabled = false
         
-        let url = URL(string: "https://google.com")
-        let req = URLRequest(url: url!)
-        browser.load(req)
+        searchText.delegate = self
+        browser.navigationDelegate = self
     }
     
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        guard let url = browser.url else {
-            return
-        }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         
-        searchText.text = url.absoluteString
+        loadURL()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        // hide keyboard
         if (searchText.isFirstResponder) {
-            searchText.resignFirstResponder()
+            searchText.resignFirstResponder() // hide keyboard
         }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField != searchText {
+            return true
+        }
+        if let link = textField.text {
+            loadURL(link)
+        }
+
+        return true
+    }
+
+    func showAlert(_ message: String) {
+        let sheet = UIAlertController(title: message, message: nil, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        sheet.addAction(action)
+        
+        present(sheet, animated: true, completion: nil)
+    }
+
+
+}
+
+extension String {
+    func quote() -> String {
+        return addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
+    }
+}
+
+extension BrowserViewController {
+    func loadURL() {
+        browser.load(URLRequest(url: URL(string: defaultURL)!))
+    }
+    
+    func loadURL(_ link: String) {
+        let url = effortURL(link) ?? URL(string: "\(defaultURL)/search?q=\(link.quote())")
+        browser.load(URLRequest(url: url!))
+    }
+    
+    // best effort
+    func effortURL(_ link: String) -> URL? {
+        if URL(string: link)?.scheme != nil {
+            return URL(string: link)
+        }
+        if verifyURL("https://\(link)") {
+            return URL(string: "https://\(link)")
+        }
+        if verifyURL("http://\(link)") {
+            return URL(string: "http://\(link)")
+        }
+        
+        return nil
+    }
+
+    func verifyURL(_ link: String?) -> Bool {
+        guard let link = link, let url = URL(string: link) else {
+            return false
+        }
+        if (url.host?.components(separatedBy: ".").count ?? 0) < 2 {
+           return false
+        }
+        
+        return UIApplication.shared.canOpenURL(url)
+    }
+}
+
+extension BrowserViewController {
+    @IBAction func goBack(_ sender: Any) {
+        browser.goBack()
+    }
+    
+    @IBAction func goFoward(_ sender: Any) {
+        browser.goForward()
+    }
+    
+    @IBAction func reload(_ sender: Any) {
+        browser.reload()
     }
     
     @IBAction func showDownload(_ sender: Any) {
@@ -52,20 +132,27 @@ class BrowserViewController: UIViewController, WKNavigationDelegate {
             (action: UIAlertAction!) in
             print("download!!")
         })
-
+        
         sheet.addAction(cancel)
         sheet.addAction(action)
         
         present(sheet, animated: true, completion: nil)
     }
-    
-    
-    // override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    //     // Get the new view controller using segue.destinationViewController.
-    //     // Pass the selected object to the new view controller.
-    // }
-
-    
-    
 }
 
+extension BrowserViewController {
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+    }
+    
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        backButton.isEnabled = browser.canGoBack
+        forwardButton.isEnabled = browser.canGoForward
+    }
+    
+    // Set current url to search text
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        if let url = browser.url {
+            searchText.text = url.absoluteString
+        }
+    }
+}
