1e10caf7ee4759f467202ea852ba4b57d1382efb
ikeikeikeike <jp.ne.co.jp@gmail.com>
2018-05-24 00:52:33 +0900

    db

---
+++ Camotes/Data/DataStore/DB/FilerDB.swift
@@ -0,0 +1,9 @@
+//
+//  FilerDB.swift
+//  Camotes
+//
+//  Created by Tatsuo Ikeda on 2018/05/24.
+//  Copyright © 2018 Tatsuo Ikeda. All rights reserved.
+//
+
+import Foundation
---
+++ Camotes/Data/DataStore/FilerStore.swift
@@ -0,0 +1,9 @@
+//
+//  FilerStore.swift
+//  Camotes
+//
+//  Created by Tatsuo Ikeda on 2018/05/24.
+//  Copyright © 2018 Tatsuo Ikeda. All rights reserved.
+//
+
+import Foundation
