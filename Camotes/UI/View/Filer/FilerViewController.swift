e8a5ef3cd55e617209299ece6191cbf57ed73240
ikeikeikeike <jp.ne.co.jp@gmail.com>
2018-06-17 15:14:37 +0900

    download video

--- Camotes/UI/View/Filer/FilerViewController.swift
+++ Camotes/UI/View/Filer/FilerViewController.swift
@@ -11,8 +11,13 @@ import AVKit
 import AVFoundation
 
 
-let unko = "http://localhost:8000/api/stream/aHR0cHM6Ly93d3cueHZpZGVvcy5jb20vdmlkZW8zNjE1MzEyNy8zMTI2NTg3LzAvc2xpbV9tb21fcG9zaXRpb25zX2hlcl90aWdodF9jdW50X29uX2hhcmRfY29ja193aGlsc3RfZGVlcHRocm9hdGluZw==?format=mp4-low"
+//let unko = "http://localhost:8000/api/stream/aHR0cHM6Ly93d3cueHZpZGVvcy5jb20vdmlkZW8zNjE1MzEyNy8zMTI2NTg3LzAvc2xpbV9tb21fcG9zaXRpb25zX2hlcl90aWdodF9jdW50X29uX2hhcmRfY29ja193aGlsc3RfZGVlcHRocm9hdGluZw==?format=mp4-low"
 
+//let unko = "http://www.gomplayer.jp/img/sample/mp4_h264_aac.mp4"
+
+let unko = "http://localhost:8000/api/stream/aHR0cHM6Ly93d3cueHZpZGVvcy5jb20vdmlkZW8yODgzMjA3MS9zaGFwZWx5X2xhdGluYV9pbl90YW5fY29sb3JlZF9zdXBlcl9zaG9ydF9kcmVzcw==?format=mp4-low"
+
+//let unko = "http://localhost:1234/b4d07134-0db4-4a38-b8c5-f3adace80e46.mp4"
 
 class FilerViewController: UIViewController {
     
@@ -45,10 +50,10 @@ class FilerViewController: UIViewController {
         
         // Create an AVPlayer, passing it the HTTP Live Streaming URL.
         //        let asset = AVURLAsset(url: url)
-        let item = CachingPlayerItem(url: url)
-        item.delegate = self
+        let playerItem = CachingPlayerItem(url: url)
+        playerItem.delegate = self
         
-        player = AVPlayer(playerItem: item)
+        player = AVPlayer(playerItem: playerItem)
         player.automaticallyWaitsToMinimizeStalling = false
         
         // Create a new AVPlayerViewController and pass it a reference to the player.
@@ -57,7 +62,7 @@ class FilerViewController: UIViewController {
         
         // Modally present the player and call the player's play() method when complete.
         present(controller, animated: true) {
-            self.player.playImmediately(atRate: 1.0)
+            playerItem.download() //  playerItem.play()
         }
     }
 }
@@ -66,11 +71,17 @@ extension FilerViewController: CachingPlayerItemDelegate {
     
     func playerItem(_ playerItem: CachingPlayerItem, didFinishDownloadingData data: Data) {
         print("File is downloaded and ready for storing")
-        player.playImmediately(atRate: 1.0)
+        player.play()
     }
     
     func playerItem(_ playerItem: CachingPlayerItem, didDownloadBytesSoFar bytesDownloaded: Int, outOf bytesExpected: Int) {
         print("\(bytesDownloaded)/\(bytesExpected)")
+    }
+
+    // Is called after initial prebuffering is finished, means we are ready to play.
+    func playerItemReadyToPlay(_ playerItem: CachingPlayerItem) {
+        print("we are ready to play")
+        player.playImmediately(atRate: 1.0)
     }
     
     func playerItemPlaybackStalled(_ playerItem: CachingPlayerItem) {
@@ -78,6 +89,6 @@ extension FilerViewController: CachingPlayerItemDelegate {
     }
     
     func playerItem(_ playerItem: CachingPlayerItem, downloadingFailedWith error: Error) {
-        print(error)
+        print("play error: ", error)
     }
 }
